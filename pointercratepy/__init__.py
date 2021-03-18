8a7ca593473cf4794a5e895a638b545b45a5d125
bretheskevin <bretheskevin@gmail.com>
2021-03-18 18:32:40 +0100

    Change DOCSTRING to Google Format

--- pointercratepy/__init__.py
+++ pointercratepy/__init__.py
@@ -9,31 +9,28 @@ class Client:
     @staticmethod
     def get_demons(limit=50, name="", name_contains="", after=0, before=0, verifier_id=0, publisher_id=0,
                    publisher_name="", listed=True):
-        api_v2 = "https://pointercrate.com/api/v2/"
         """
-        :param
-        - limit: The maximum amount of object to return. Must lie between 1 and 100. Default is 50.
-        - name: Filter with the name of the demon [!!!] Case sensitive [!!!]
-        - name_contains: Check if a demon has the specified string in his name, not case sensitive 
-        so it's a good alternative to name filter.
-        
-        - after: Used for pagination, here is an example:
-                    limit = 100: You will get the top 100 levels of the list (listed is True by default)
-                    limit = 100 AND after = 100: You will get the levels from position 101 to 200 because
-                it will skip the 100 first ones.        
-        - before: Also used for pagination, here is an example:
-                    limit = 100 AND before = 6 : You will get the 5 first levels
-        You can use before and after to easily filter by the position:
-            after=9 and before=25: 10th to 24th levels            
-        
-        - verifier_id: Filter with the verifier's id
-        - publisher_id: Filter with the publisher's id
-        - publisher_name: Filter with the name of the player who uploaded the level
-        - listed: Sort the levels by position. Default is True
 
-        :return:
-        - List of objects containing all demons information.
-        """
+        Args:
+            limit: The maximum amount of object to return. Must lie between 1 and 100. Default is 50.
+            name: Filter with the name of the demon [!!!] Case sensitive [!!!]
+            name_contains: Check if a demon has the specified string in his name, not case sensitive so it's a good alternative to name filter.
+            after: Used for pagination, here is an example:
+                    limit = 100: You will get the top 100 levels of the list (listed is True by default)
+                    limit = 100 AND after = 100: You will get the levels from position 101 to 200 because it will skip the 100 first ones.
+            before: Also used for pagination, here is an example:
+                        - limit = 100 AND before = 6 : You will get the 5 first levels
+                    You can use before and after to easily filter by the position:
+                        - after=9 and before=25: 10th to 24th levels
+            verifier_id: Filter with the verifier's id
+            publisher_id: Filter with the publisher's id
+            publisher_name: Filter with the name of the player who uploaded the level
+            listed: Sort the levels by position. Default is True
+
+        Returns:
+            list: List of objects containing all demons information.
+          """
+        api_v2 = "https://pointercrate.com/api/v2/"
 
         # Handling errors
         if limit < 1 or limit > 100:
